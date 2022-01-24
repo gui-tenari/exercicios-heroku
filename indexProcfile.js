b201dfbdf9decabcae1275a78548d915cabe8d8f
Joao Victor <joaogmavictor@gmail.com>
2022-01-24 19:25:51 -0300

    Adicionando procfile

--- indexProcfile.js
+++ indexProcfile.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;  
 
-app.get('/', (req, res) => res.send('ESTOU VIVO!!!'))
+app.get('/', (req, res) => res.send("Procfile funciona mesmo!"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
