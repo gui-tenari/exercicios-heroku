c7c16b86d65569bcc67690ce7eecebbd131355de
ricand7 <ricantonioj@hotmail.com>
2022-01-24 19:15:40 -0300

    versao 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;  
+const varenv = process.env.VAR
 
-app.get('/', (req, res) => res.send('ESTOU VIVO!!!'))
+app.get('/', (req, res) => res.send(`ESTOU VIVO!!!${process.env.TEXTO}`))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
